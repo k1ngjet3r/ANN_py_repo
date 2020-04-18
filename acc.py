cebeebac97f895f48db2b780d0bcc6d7c8e64b13
K1NGJET3R <kingice00@gmail.com>
2020-04-18 19:52:01 +0800

    20190418_corrections

--- acc.py
+++ acc.py
@@ -1,16 +1,13 @@
-import random
-from math import exp
 import numpy as np
-import mnist_data_loader
-from mnist_data_loader import test_input, train_input
+import random as rd
 
 
-def Sigmoid(x):
-    return 1 / (1 + exp(-x))
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
 
-def diff_sigmoid(x):
-    return Sigmoid(x) * (1 - Sigmoid(x))
+def diff_sigmoid(z):
+    return sigmoid(z) * (1 - sigmoid(z))
 
 
 class Network:
@@ -22,76 +19,85 @@ class Network:
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
     def backprop(self, x, y):
+        # forming all the activations of each layers
+        activation = x
+        activations = [x]
         zs = []
-        act = x
-        acts = [x]
-        # feed forward
+        d_zs = []
         for w, b in zip(self.weights, self.biases):
-            z = [sum(x) for x in zip(np.dot(w, act), b)]
-            z = np.dot(w, act) + b
-            zs.append(z)  # zs = [z1, z2, z3, ..., zL]
-            act = [Sigmoid(zz) for zz in z]
-            acts.append(act)  # acts = [a0, a1, a2, ..., aL]
+            z = np.reshape(np.dot(w, activation), (len(b), 1)) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+            d_z = diff_sigmoid(z)
+            d_zs.append(d_z)
+        # the error for layer L (last layer)
+        error_output = (activations[-1] - y) * d_zs[-1]
+        errors = [error_output]
 
-        # back propagate
-        # last hidden layer
-        deltas = []
-        cost_diff_a = acts[-1] - y
-        Delta = np.dot(cost_diff_a, diff_sigmoid(zs[-1]))
-        deltas.append(Delta)  # deltas = [dL]
+        # finding errors for rest of the layers (L-1, L-2, ... , 2, 1)
+        L = self.num_layers
+        for i in range(1, L - 1):
+            tran_w = np.transpose(self.weights[-i])
+            inner = [np.dot(w, errors[-1]) for w in tran_w]
+            error = [inn * dz for inn, dz in zip(inner, d_zs[-i - 1])]
+            errors.append(error)
+        # errors.reverse()
 
-        # other hidden layers
-        for j in range(1, self.num_layers):
-            inner_prod = np.dot(self.weights[-j].transpose(), deltas[-1])
-            delta = np.dot(inner_prod, diff_sigmoid(zs[-j - 1]))
-            deltas.append(delta)  # equals to nebla_b
+        # the gradient of weights and biases
+        grad_ws = []
+        grad_bs = errors
+        for i in range(L - 1):
+            grad_w = [e * activations[i] for e in errors[i]]
+            grad_ws.append(grad_w)
+        # reshape the grad_ws matrix
+        reshape_grad_ws = []
+        for j in range(L - 1):
+            reshape_grad_ws.append(
+                np.vstack([np.transpose(gw) for gw in grad_ws[i]]))
 
-        # nebla_w
-        nebla_w = [
-            np.dot(acts[-k - 1], deltas[k - 1]) for k in range(1, self.num_layers)
-        ]
+        return (reshape_grad_ws, grad_bs)
 
-        # nebla_b
-        nebla_b = deltas
-
-        return (nebla_w, nebla_b)
-
-    def update_mini_batch(self, mini_batch, learning_rate):
+    def update_mini_batch(self, mini_batch, eta):
         for y, x in mini_batch:
-            nebla_w, nebla_b = self.backprop(x, y)
-            self.weights = [w - (learning_rate / len(mini_batch))
-                            * n for w, nw in zip(self.weights, nebla_w)]
-            self.biases = [b - (learning_rate / len(mini_batch))
-                           * nb for b, nb in zip(self.biases, nebla_b)]
-
-  # Stochastic gradient descent
-    def SGD(self, training_data, epoches, mini_batch_size, eta, test_data=None):
-        if test_data:
-            n_test = len(test_data)
-        n = len(training_data)
-        for j in range(epoches):
-            random.shuffle(training_data)
-            mini_batches = [training_data[k: k + mini_batch_size]
-                            for k in range(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print("Epoch {0}: {1} / {2}".format(j,
-                                                    self.evaluate(test_data), n_test))
-            else:
-                print('Epoch {0} complete'.format(j))
+            d_w, d_b = self.backprop(x, y)
+            self.weights = [w - (eta/len(mini_batch)) *
+                            dw for w, dw in zip(self.weights, d_w)]
+            self.biases = [b - (eta/len(mini_batch)) *
+                           db for b, db in zip(self.biases, d_b)]
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = Sigmoid(np.dot(w, a) + b)
+            a = sigmoid(np.dot(w, a) + b)
             return a
 
     def evaluate(self, test_data):
-        for y, x in test_data:
-            test_results = [(np.argmax(self.feedforward(x)), y)]
-        return sum([int(x == y) for (x, y) in test_results])
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for x, y in test_data]
+        return sum([int(x == y) for x, y in test_results])
+
+    def SGD(self, training_data, mini_batch_size, epoches, eta, test_data=None):
+        if test_data:
+            n_test = len(test_data)
+        n_train = len(training_data)
+
+        for i in range(epoches):
+            rd.shuffle(training_data)
+            mini_batches = [training_data[j: j + mini_batch_size]
+                            for j in range(0, n_train, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+
+            if test_data:
+                print('Epoch {0}: {1} / {2}'.format(i,
+                                                    self.evaluate(test_data), n_test))
+            else:
+                print('Epoch {0} complete'.format(i))
 
 
-# Execution
-net = Network([784, 30, 10])
-net.SGD(train_input, 30, 10, 3.0, test_data=test_input)
+# net = Network([2, 3, 4, 1])
+# input_act = np.array([[1], [0]])
+# desired_output = np.array([1])
+# print(net.weights)
+# print('------------------------------')
+# net.backprop(input_act, desired_output)
---
+++ heist.py
@@ -0,0 +1,77 @@
+# import os
+# from selenium import webdriver
+# import datetime
+# import time
+
+# option = webdriver.ChromeOptions()
+# option.add_argument("--diaable-infobars")
+# option.add_argument("--disable-notifications")
+# browser = webdriver.Chrome(
+#     '/Users/jeter/Downloads/chromedriver', chrome_options=option)
+
+# browser.maximize_window()
+# print('Opening Chrome...')
+# browser.get('https://shopee.tw/product-i.127651399.2670033811')
+# time.sleep(2)
+# print('Opened the page successfully, trying to log-in to the website...')
+# login_ele = browser.find_element_by_xpath(
+#     "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/div/div/div/div[1]/div/ul/li[5]")
+# login_ele.click()
+# time.sleep(2)
+
+# print('entering the user infomation')
+
+# username = browser.find_element_by_xpath(
+#     '/html/body/div[2]/aside/div[1]/div/div/div/div[2]/div[1]/div[2]/div/input')
+# password = browser.find_element_by_xpath(
+#     '/html/body/div[2]/aside/div[1]/div/div/div/div[2]/div[1]/div[3]/div/input')
+# submit = browser.find_element_by_xpath(
+#     '/html/body/div[2]/aside/div[1]/div/div/div/div[2]/div[2]/button[2]')
+
+# username.send_keys('0928590619')
+# password.send_keys('xbox360')
+# submit.click()
+
+# print('account log-in successful')
+# time.sleep(10)
+
+
+# # print(browser.current_url)
+# # target = browser.find_element_by_xpath(
+# #     '/html/body/div[1]/div/div[2]/div/div[2]/div/div[14]/div/div/div/div/div[4]')
+# # target.click()
+import numpy as np
+
+b = np.array([[-0.39295413],
+              [0.5131677],
+              [1.95352453],
+              [0.30982339],
+              [0.10793655],
+              [-0.08660033],
+              [1.50303502],
+              [-2.34255143],
+              [0.34313784],
+              [0.79029169],
+              [-0.16205514],
+              [-0.71151008],
+              [0.34866291],
+              [0.94227584],
+              [-0.43045028],
+              [0.69408095],
+              [-1.10605531],
+              [-0.54086024],
+              [-0.2718316],
+              [-0.94229782],
+              [1.43274685],
+              [-0.17001073],
+              [0.75693747],
+              [-1.12525984],
+              [1.19840558],
+              [1.01053424],
+              [0.15032947],
+              [-0.10604911],
+              [-0.36753689],
+              [-0.49584128]])
+print(b.shape)
+np.reshape(b, (len(b),))
+print(b.shape)
