0d1d04efe2058a4174e650775d19e85c26221b06
K1NGJET3R <kingice00@gmail.com>
2020-04-28 10:03:12 +0800

    old

--- heist.py
+++ heist.py
@@ -3,23 +3,26 @@ import tensorflow as tf
 from tensorflow import keras
 
 fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images,
-                               test_labels) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-model = keras.Sequential([
-    keras.layers.Flatten(),
-    keras.layers.Dense(64, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+model = keras.Sequential(
+    [
+        keras.layers.Flatten(),
+        keras.layers.Dense(64, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax),
+    ]
+)
 
-model.compile(optimizer=keras.optimizers.Adam(),
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"],
+)
 
-model.fit(train_images, train_labels, epochs=5)
+model.fit(train_images, train_labels, epochs=30)
 
 model.evaluate(test_images, test_labels)
 
