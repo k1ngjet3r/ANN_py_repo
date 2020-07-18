30ac21f6d528256fed3ea93d2d56683a16adb7b4
K1NGJET3R <kingice00@gmail.com>
2020-07-19 07:48:53 +0800

    new code local testing

--- Artificial_Neural_Network.py
+++ Artificial_Neural_Network.py
@@ -6,9 +6,10 @@ from random import shuffle
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
 fraction = 0.99/255.0
-x_train, x_test = (np.around(x_train * fraction, decimals=2) +
-                   0.01), (np.around(x_test * fraction, decimals=2) + 0.01)
+x_train = x_train * fraction + 0.01
+x_test = x_test * fraction + 0.01
 
 desired_y_train = []
 desired_y_test = []
@@ -28,25 +29,30 @@ def data_formater(x, y):
     return [(xx, yy) for xx, yy in zip(x, y)]
 
 
-training_data = data_formater(x_train, y_train)
-testing_data = data_formater(x_test, y_test)
+training_data = data_formater(x_train, desired_y_train)
+testing_data = data_formater(x_test, desired_y_test)
+
+# for ReLu activation function
 
 
-def Sigmoid(z):
-    return 1 / (1 + exp(-z))
+def activation_function(z):
+    return max(0, z)
 
 
-def diff_Sigmoid(z):
-    return Sigmoid(z) * (1 - Sigmoid(z))
+def diff_activation_function(z):
+    if z > 0:
+        return 1
+    else:
+        return 0
 
 
 class Network:
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.around(np.random.randn(y, 1), decimals=2)
-                       for y in sizes[1:]]
-        self.weights = [np.around(np.random.randn(y, x), decimals=2)
+        self.biases = [
+            np.around(0.01 * np.random.randn(y, 1), decimals=8) for y in sizes[1:]]
+        self.weights = [np.around(0.01 * np.random.randn(y, x), decimals=8)
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
     # Function that calculate and forming the output layer neurons from inputs
@@ -58,21 +64,23 @@ class Network:
             weighted_sum = np.add(
                 np.dot(self.weights[i], activation), self.biases[i].flatten())
             zs.append(weighted_sum)
-            activation = np.array([Sigmoid(z) for z in weighted_sum])
+            activation = np.array([activation_function(z)
+                                   for z in weighted_sum])
             activations.append(activation)
-        return activations, zs
+        return (activations, zs)
 
     def error_generator(self, ann_network, zs):
         errors = []
-        d_sigmoid_z = [np.array([diff_Sigmoid(zij) for zij in z]) for z in zs]
+        d_activation_z = [
+            np.array([diff_activation_function(zij) for zij in z]) for z in zs]
         gradient_L = np.dot(self.weights[-1].transpose(), ann_network[-1])
         backward_errors = [
-            np.array([g * z for g, z in zip(gradient_L, d_sigmoid_z[-1])])]
+            np.array([g * z for g, z in zip(gradient_L, d_activation_z[-1])])]
         for i in range(1, self.num_layers - 1):
             gradient_l = np.dot(
                 self.weights[-i].transpose(), backward_errors[-1])
             error_l = np.array(
-                [g * z for g, z in zip(gradient_l, d_sigmoid_z[-i - 1])])
+                [g * z for g, z in zip(gradient_l, d_activation_z[-i - 1])])
             backward_errors.append(error_l)
 
         for j in range(len(backward_errors)):
@@ -82,7 +90,7 @@ class Network:
         nabla_w = [np.array([(ann_network[i] * d).tolist()
                              for d in errors[i]]) for i in range(net.num_layers - 1)]
 
-        return nabla_b, nabla_w
+        return (nabla_b, nabla_w)
 
     def update_weights_and_biases(self, nabla_w, nabla_b, learning_rate, data_mini_batch_size):
         self.weights = [w - (learning_rate/data_mini_batch_size)
@@ -92,8 +100,8 @@ class Network:
         # return self.weights, self.biases
 
     def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feed_forward(x)[0][-1]), y)
-                        for (x, y) in test_data]
+        test_results = [(np.argmax(self.feed_forward(x)[0][-1]),
+                         np.argmax(y)) for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
     def SGD(self, training_data, batch_size, epoch, learning_rate, test_data=None):
@@ -101,6 +109,7 @@ class Network:
             n_test = len(test_data)
         n = len(training_data)
         for i in range(epoch):
+            print('running epoch no. {0}'.format(i + 1))
             shuffle(training_data)
             mini_batches = [training_data[k: k + batch_size]
                             for k in range(0, n, batch_size)]
@@ -118,4 +127,5 @@ class Network:
 
 
 net = Network([784, 30, 10])
-net.SGD(training_data, 10, 10, 3.0, test_data=testing_data)
+net.SGD(training_data, batch_size=10, epoch=30,
+        learning_rate=10.0, test_data=testing_data)
