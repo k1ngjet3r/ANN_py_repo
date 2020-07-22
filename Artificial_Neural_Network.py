2fa35f7ebc17d6a35a25e50d9d04d1a24de03c51
K1NGJET3R <kingice00@gmail.com>
2020-07-22 15:28:47 +0800

    Major update of the ann model using sigmoid as activation function

--- Artificial_Neural_Network.py
+++ Artificial_Neural_Network.py
@@ -25,41 +25,41 @@ for j in y_test:
     desired_y_test.append(zero)
 
 
-def data_formater(x, y):
+def data_formatter(x, y):
     return [(xx, yy) for xx, yy in zip(x, y)]
 
 
-training_data = data_formater(x_train, desired_y_train)
-testing_data = data_formater(x_test, desired_y_test)
-
-# for ReLu activation function
+training_data = data_formatter(x_train, desired_y_train)
+testing_data = data_formatter(x_test, desired_y_test)
 
 
 def activation_function(z):
-    return max(0, z)
+    return 1 / (1 + exp(-z))
 
 
 def diff_activation_function(z):
-    if z > 0:
-        return 1
-    else:
-        return 0
+    return activation_function(z) * (1 - activation_function(z))
 
 
 class Network:
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [
-            np.around(0.01 * np.random.randn(y, 1), decimals=8) for y in sizes[1:]]
-        self.weights = [np.around(0.01 * np.random.randn(y, x), decimals=8)
+        self.biases = [np.random.randn(y, 1).flatten() for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
-    # Function that calculate and forming the output layer neurons from inputs
-    def feed_forward(self, input_layer):
+    def backprop(self, input_layer, desired_output):
+        # setting the shape of the nabla_b and nabla_w
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # Setting up the required list
+        activation = input_layer.flatten()
         activations = [input_layer.flatten()]
-        activation = np.array(input_layer.flatten())
         zs = []
+
+        # Feeding forward and we got the full picture of the network including every neurons (a) and weighted sum (z) in each layers
         for i in range(self.num_layers - 1):
             weighted_sum = np.add(
                 np.dot(self.weights[i], activation), self.biases[i].flatten())
@@ -67,28 +67,24 @@ class Network:
             activation = np.array([activation_function(z)
                                    for z in weighted_sum])
             activations.append(activation)
-        return (activations, zs)
 
-    def error_generator(self, ann_network, zs):
-        errors = []
-        d_activation_z = [
-            np.array([diff_activation_function(zij) for zij in z]) for z in zs]
-        gradient_L = np.dot(self.weights[-1].transpose(), ann_network[-1])
-        backward_errors = [
-            np.array([g * z for g, z in zip(gradient_L, d_activation_z[-1])])]
-        for i in range(1, self.num_layers - 1):
-            gradient_l = np.dot(
-                self.weights[-i].transpose(), backward_errors[-1])
-            error_l = np.array(
-                [g * z for g, z in zip(gradient_l, d_activation_z[-i - 1])])
-            backward_errors.append(error_l)
+        # Backward pass, last layer first
+        delta = (activations[-1] - desired_output) * \
+            np.array([diff_activation_function(z) for z in zs[-1]])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.array([(i * activations[-2].transpose()).tolist()
+                                for i in delta])
 
-        for j in range(len(backward_errors)):
-            errors.append(backward_errors[-1 - j])
-
-        nabla_b = [np.array([[j] for j in i]) for i in errors]
-        nabla_w = [np.array([(ann_network[i] * d).tolist()
-                             for d in errors[i]]) for i in range(net.num_layers - 1)]
+        # Other layers
+        for j in range(2, self.num_layers):
+            z = zs[-j]
+            activation_prime = np.array(
+                [diff_activation_function(zz) for zz in z])
+            delta = np.dot(
+                self.weights[-j + 1].transpose(), delta) * activation_prime
+            nabla_b[-j] = delta
+            nabla_w[-j] = np.array([(i * activations[-j -
+                                                     1].transpose()).tolist() for i in delta])
 
         return (nabla_b, nabla_w)
 
@@ -100,9 +96,16 @@ class Network:
         # return self.weights, self.biases
 
     def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feed_forward(x)[0][-1]),
-                         np.argmax(y)) for (x, y) in test_data]
+        test_results = [(np.argmax(self.feed_forward(x)), np.argmax(y))
+                        for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
+
+    def feed_forward(self, a):
+        a = a.flatten()
+        for b, w in zip(self.biases, self.weights):
+            weighted_sum = np.add(np.dot(w, a), b)
+            a = np.array([activation_function(z) for z in weighted_sum])
+        return a
 
     def SGD(self, training_data, batch_size, epoch, learning_rate, test_data=None):
         if test_data != None:
@@ -115,17 +118,18 @@ class Network:
                             for k in range(0, n, batch_size)]
             for mini_batch in mini_batches:
                 for x, y in mini_batch:
-                    activations, zs = self.feed_forward(x)
-                    nabla_b, nabla_w = self.error_generator(activations, zs)
+                    nabla_b, nabla_w = self.backprop(x, y)
                     self.update_weights_and_biases(
                         nabla_w, nabla_b, learning_rate, batch_size)
             if test_data:
                 print('Epoch {0}: {1} / {2}'.format(i,
                                                     self.evaluate(test_data), n_test))
+                print('Accuracy: {0}%'.format(
+                    (self.evaluate(test_data) / n_test)*100))
             else:
                 print('Epoch {0} complete'.format(i))
 
 
 net = Network([784, 30, 10])
 net.SGD(training_data, batch_size=10, epoch=30,
-        learning_rate=10.0, test_data=testing_data)
+        learning_rate=3.0, test_data=testing_data)
